ac56d099575f3b609266735d2c96307279bf2ca5
Genollium <vasuninilyaolegovich@gmail.com>
2023-12-05 11:36:14 +0300

    добавлена точность eps

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <windows.h>
+#define eps 1e-10
 
 //первая функция
 double first(double x) {
@@ -13,35 +14,43 @@ double second(double x) {
 }
 
 // Метод Ньютона для приближенного вычисления определенного интеграла
-double Integral(double (*func)(double), double a, double b, int n) {
-    double h = (b - a) / n; // Шаг интегрирования
-    double result = (func(a) + func(b)); // Первый и последний члены
+double Integral(double (*func)(double), double a, double b) {
+    int n = 2; // Начальное количество шагов
+    int max_iter = 1000;
+    double result = 0.0;
+    double integral_prev = 0.0;
 
-    for (int i = 1; i < n; i++) {
-        double x_i = a + i * h;
-        double y_i = func(x_i);
+    for (int iter = 0; iter < max_iter; ++iter) {
+        double h = (b - a) / n; // Шаг интегрирования
+        double sum = (func(a) + func(b)); // Первый и последний члены
 
-        if (i % 3 == 0) {
-            result += 2 * y_i; // Каждый третий член с весом 2
-        } else {
-            result += 3 * y_i; // Остальные с весом 3
+        for (int i = 1; i < n; i++) {
+            double x_i = a + i * h;
+            double y_i = func(x_i);
+            if (i % 3 == 0) {
+                sum += 2 * y_i; // Каждый третий член с весом 2
+            } else {
+                sum += 3 * y_i; // Остальные с весом 3
+            }
         }
+
+        result = (3 * h / 8) * sum;
+
+        if (fabs(result - integral_prev) < eps) {
+            return result; // Достигли заданной точности
+        }
+
+        integral_prev = result;
+        n *= 2; // Удвоение количества шагов
     }
-
-    result = (3 * h / 8) * result;
-
     return result;
 }
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
-    double eps;
-    printf("Введите точность (она должна быть кратна 3): ");
-    scanf("%lf",&eps);
-
-    double result = Integral(first, -2, 1, eps);
-    printf("Приближенное значение определенного интеграла: %lf\n", result);
-    result = Integral(second, -2, 3, eps);
-    printf("Приближенное значение определенного интеграла: %lf\n", result);
+    double result = Integral(first, -2, 1);
+    printf("Приближенное значение определенного интеграла: %.10lf\n", result);
+    result = Integral(second, -2, 3);
+    printf("Приближенное значение определенного интеграла: %.10lf\n", result);
     return 0;
 }
